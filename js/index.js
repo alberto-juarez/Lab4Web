2097a8079c1f6ba32e69a8513f8768d9183bc3d6
Alberto <aji147@gmail.com>
2020-03-12 23:36:06 -0600

    Cambios finales

--- js/index.js
+++ js/index.js
@@ -1,84 +1,44 @@
 var input =  "";
 var result = 0;
 
-document.getElementById("addButton").addEventListener('click', operador("+"));
-document.getElementById("substractButton").addEventListener('click', operacion("-"));
-document.getElementById("multiplicationButton").addEventListener('click',operacion("*"));
-document.getElementById("divisionButton").addEventListener('click',operacion("/"));
-
-// function add(){
-//   var num = document.getElementsByClassName("inputNumber")[0].value;
-//   input += num + ' + ';
-//   document.getElementsByClassName("inputNumber")[0].value = "";
-//
-// }
-//
-// function substract(){
-//   var num = document.getElementsByClassName("inputNumber")[0].value;
-//   input += num + ' - ';
-//   document.getElementsByClassName("inputNumber")[0].value = "";
-//
-// }
-//
-//
-// function multiply(){
-//   var num = document.getElementsByClassName("inputNumber")[0].value;
-//   input += num + ' * ';
-//   document.getElementsByClassName("inputNumber")[0].value = "";
-//
-// }
-//
-// function divide(){
-//   var num = document.getElementsByClassName("inputNumber")[0].value;
-//   input += num + ' / ';
-//   document.getElementsByClassName("inputNumber")[0].value = "";
-//
-// }
 
 function operacion(x){
-      var num = document.getElementsByClassName("inputNumber")[0].value;
-      switch(x){
-        case "+":
-            input += num + ' + ';
-            console.log(input);
-        break;
-
-        case "-":
-            input += num + ' - ';
-            console.log(input);
-        break;
-
-        case "*":
-            input += num + ' * ';
-            console.log(input);
-        break;
-
-        case "/":
-            input += num + ' / ';
-        break;
-      }
-
-      document.getElementsByClassName("inputNumber")[0].value = "";
-      console.log(input);
+  var num = document.getElementsByClassName("inputNumber")[0].value;
+  input += num + x;
+  document.getElementsByClassName("inputNumber")[0].value = "";
 }
+
+document.getElementById("addButton").addEventListener('click', (event) =>{
+    operacion(" + ");
+});
+
+document.getElementById("substractButton").addEventListener('click', (event) =>{
+    operacion(" - ");
+});
+
+document.getElementById("multiplicationButton").addEventListener('click', (event) =>{
+    operacion(" * ");
+});
+
+document.getElementById("divisionButton").addEventListener('click', (event) =>{
+    operacion(" / ");
+});
 
 document.getElementsByClassName("resetButton")[0].addEventListener('click', (event) =>{
     input = "";
     document.getElementsByClassName("inputNumber")[0].value = input;
     document.getElementById("resultValue").value = "";
     document.getElementsByClassName("inputNumber")[0].value = "";
-    document.getElementById("logInformation").value += input;
+    document.getElementById("logInformation").value = "";
 
 });
 
 document.getElementsByClassName("equalButton")[0].addEventListener('click', (event) => {
     input +=  document.getElementsByClassName("inputNumber")[0].value;
-    input.replace(/\s/g, "");
-    result = eval(input);
-    document.getElementById("resultValue").value = result;
-    input = input + " = " + result + "\n";
-    document.getElementById("logInformation").value += input;
     document.getElementsByClassName("inputNumber")[0].value = "";
+    result = eval(input);
+    input += " = " + result + "\n";
+    document.getElementById("resultValue").value = result;
+    document.getElementById("logInformation").value += input;
     input = "";
-
 });
